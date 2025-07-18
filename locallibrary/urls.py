5bc94378c288f75448c5719dac4b7cd300406d9e
Hoai Nam <nhnnhn1832004@gmail.com>
2025-07-18 17:41:02 +0700

    Django tutorial part 8: User authentication and permissions

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -22,4 +22,5 @@ from django.urls import include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("catalog/", include("catalog.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
