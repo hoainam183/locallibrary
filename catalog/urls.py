66bc4b0bc47863142ecd32e61667dfa1e2be8ecf
Hoai Nam <nhnnhn1832004@gmail.com>
2025-07-21 10:14:20 +0700

    Django Tutorial Part 9: Working with forms

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,20 @@ urlpatterns = [
         views.LoanedBooksByUserListView.as_view(),
         name="my-borrowed",
     ),
+    path(
+        "book/<uuid:pk>/renew/",
+        views.renew_book_librarian,
+        name="renew-book-librarian",
+    ),
+    path("author/create/", views.AuthorCreate.as_view(), name="author-create"),
+    path(
+        "author/<int:pk>/update/",
+        views.AuthorUpdate.as_view(),
+        name="author-update",
+    ),
+    path(
+        "author/<int:pk>/delete/",
+        views.AuthorDelete.as_view(),
+        name="author-delete",
+    ),
 ]
